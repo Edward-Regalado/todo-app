8391d9abb37ba7a18fa3f38efff4c97dc78203fa
Edward-Regalado <tregalado30@gmail.com>
2022-10-21 01:25:32 +0000

    added setup files and folder structure

---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Nav from '../components/Nav';
+import Splash from '../components/Splash';
+// import Footer from '../components/Footer';
+import Footer from '../components/Footer';
+
+function HomePage() {
+  return (
+    <>
+        <Nav />
+        <Splash />
+        <Footer />
+    </>
+  );
+}
+
+export default HomePage;
